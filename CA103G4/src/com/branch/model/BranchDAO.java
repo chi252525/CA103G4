80c9254df00cdcaeb0eed12344089a3b3d5c4c94
b2778173 <40678827+b2778173@users.noreply.github.com>
2018-09-05 23:33:46 +0800

    BranchDAO is in progress.
    0905

--- CA103G4/src/com/branch/model/BranchDAO.java
+++ CA103G4/src/com/branch/model/BranchDAO.java
@@ -1,5 +1,35 @@
 package com.branch.model;
 
-public class BranchDAO {
+import java.util.List;
 
+public class BranchDAO implements BranchDAO_interface{
+	String driver = "oracle.jdbc.driver.OracleDriver";
+	String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	String userid = "Test";
+	String passwd = "123456";
+	@Override
+	public void insert(BranchVO branchVO) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void update(BranchVO branchVO) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void delete(String branchVO) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public BranchVO findByPrimaryKey(String branch_No) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public List<BranchVO> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
