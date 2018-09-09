277df5da905e7c9115c8aadfb751a8f3e1692192
bamboo <a3551343@gmail.ocm>
2018-09-09 22:12:00 +0800

    ok but not test*3
    for
    orderform
    orderinvoice
    couponhistory

---
+++ CA103G4/src/com/orderform/model/OrderformJDBCDAO.java
@@ -0,0 +1,298 @@
+package com.orderform.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderformJDBCDAO implements OrderformDAO_interface {
+	String driver = "oracle.jdbc.driver.OracleDriver";
+	String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	String userid = "Pro";
+	String passwd = "123456";
+
+//	private static final String INSERT_STMT = "INSERT INTO orderform (order_no,dek_no,mem_no,branch_no,deliv_no,order_type,order_price,order_status,deliv_addres,order_pstatus) values ('O'||LPAD(to_char(oredrform_seq.NEXTVAL), 9, '0'), ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	private static final String INSERT_STMT = "INSERT INTO orderform (order_no,dek_no,mem_no,branch_no,deliv_no,order_type,order_price,order_status,deliv_addres,order_pstatus) values ('0'||LPAD(to_char(oredrform_seq.NEXTVAL), 8, '0'), ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	private static final String GET_ALL_STMT = "SELECT order_no,dek_no,mem_no,branch_no,deliv_no,order_type,order_price,order_status,deliv_addres,order_pstatus FROM orderform order by order_no";
+	private static final String GET_ONE_STMT = "SELECT order_no,dek_no,mem_no,branch_no,deliv_no,order_type,order_price,order_status,deliv_addres,order_pstatus FROM orderform where order_no = ?";
+	private static final String DELETE = "DELETE FROM orderform where order_no = ?";
+	private static final String UPDATE = "UPDATE orderform set dek_no=?, mem_no=?, branch_no=?, deliv_no=?, order_type=?, order_price=?, order_status=?, deliv_addres=?, order_pstatus=? where order_no = ?";
+
+	@Override
+	public void insert(OrderformVO orderformVO) {
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(INSERT_STMT);
+
+			pstmt.setString(1, orderformVO.getDek_no());
+			pstmt.setString(2, orderformVO.getMem_no());
+			pstmt.setString(3, orderformVO.getBranch_no());
+			pstmt.setString(4, orderformVO.getDeliv_no());
+			pstmt.setInt(5, orderformVO.getOrder_type());
+			pstmt.setInt(6, orderformVO.getOrder_price());
+			pstmt.setInt(7, orderformVO.getOrder_status());
+			pstmt.setString(8, orderformVO.getDeliv_addres());
+			pstmt.setInt(9, orderformVO.getOrder_pstatus());
+
+			pstmt.executeUpdate();
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. " + se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public void update(OrderformVO orderformVO) {
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(UPDATE);
+
+			pstmt.setString(1, orderformVO.getDek_no());
+			pstmt.setString(2, orderformVO.getMem_no());
+			pstmt.setString(3, orderformVO.getBranch_no());
+			pstmt.setString(4, orderformVO.getDeliv_no());
+			pstmt.setInt(5, orderformVO.getOrder_type());
+			pstmt.setInt(6, orderformVO.getOrder_price());
+			pstmt.setInt(7, orderformVO.getOrder_status());
+			pstmt.setString(8, orderformVO.getDeliv_addres());
+			pstmt.setInt(9, orderformVO.getOrder_pstatus());
+			pstmt.setString(10, orderformVO.getOrder_no());
+
+			pstmt.executeUpdate();
+
+			// Handle any driver errors
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. " + se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public void delete(String order_no) {
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(DELETE);
+
+			pstmt.setString(1, order_no);
+
+			pstmt.executeUpdate();
+
+			// Handle any driver errors
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. " + se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public OrderformVO findByPrimaryKey(String order_no) {
+
+		OrderformVO orderformVO = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(GET_ONE_STMT);
+
+			pstmt.setString(1, order_no);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				// deliveryVO 也稱為 Domain objects
+				orderformVO = new OrderformVO();
+				orderformVO.setOrder_no(rs.getString("order_no"));
+				orderformVO.setDek_no(rs.getString("dek_no"));
+				orderformVO.setMem_no(rs.getString("mem_no"));
+				orderformVO.setBranch_no(rs.getString("branch_no"));
+				orderformVO.setDeliv_no(rs.getString("deliv_no"));
+				orderformVO.setOrder_type(rs.getInt("order_type"));
+				orderformVO.setOrder_type(rs.getInt("order_price"));
+				orderformVO.setOrder_status(rs.getInt("order_status"));
+				orderformVO.setDeliv_addres(rs.getString("deliv_addres"));
+				orderformVO.setOrder_pstatus(rs.getInt("order_pstatus"));
+
+			}
+
+			// Handle any driver errors
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. " + se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return orderformVO;
+	}
+
+	@Override
+	public List<OrderformVO> getAll() {
+		List<OrderformVO> list = new ArrayList<OrderformVO>();
+		OrderformVO orderformVO = null;
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(GET_ALL_STMT);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				// empVO 也稱為 Domain objects
+				orderformVO = new OrderformVO();
+				orderformVO.setOrder_no(rs.getString("order_no"));
+				orderformVO.setDek_no(rs.getString("dek_no"));
+				orderformVO.setMem_no(rs.getString("mem_no"));
+				orderformVO.setBranch_no(rs.getString("branch_no"));
+				orderformVO.setDeliv_no(rs.getString("deliv_no"));
+				orderformVO.setOrder_type(rs.getInt("order_type"));
+				orderformVO.setOrder_type(rs.getInt("order_price"));
+				orderformVO.setOrder_status(rs.getInt("order_status"));
+				orderformVO.setDeliv_addres(rs.getString("deliv_addres"));
+				orderformVO.setOrder_pstatus(rs.getInt("order_pstatus"));
+
+				list.add(orderformVO); // Store the row in the list
+			}
+
+			// Handle any driver errors
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. " + se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return list;
+	}
+
+}
---
+++ CA103G4/src/com/orderform/model/OrderformVO.java
@@ -0,0 +1,74 @@
+package com.orderform.model;
+
+public class OrderformVO {
+	private String order_no;
+	private String dek_no;
+	private String mem_no;
+	private String branch_no;
+	private String deliv_no;
+	private Integer order_type;
+	private Integer order_price;
+	private Integer order_status;
+	private String deliv_addres;
+	private Integer order_pstatus;
+	public String getOrder_no() {
+		return order_no;
+	}
+	public void setOrder_no(String order_no) {
+		this.order_no = order_no;
+	}
+	public String getDek_no() {
+		return dek_no;
+	}
+	public void setDek_no(String dek_no) {
+		this.dek_no = dek_no;
+	}
+	public String getMem_no() {
+		return mem_no;
+	}
+	public void setMem_no(String mem_no) {
+		this.mem_no = mem_no;
+	}
+	public String getBranch_no() {
+		return branch_no;
+	}
+	public void setBranch_no(String branch_no) {
+		this.branch_no = branch_no;
+	}
+	public String getDeliv_no() {
+		return deliv_no;
+	}
+	public void setDeliv_no(String deliv_no) {
+		this.deliv_no = deliv_no;
+	}
+	public Integer getOrder_type() {
+		return order_type;
+	}
+	public void setOrder_type(Integer order_type) {
+		this.order_type = order_type;
+	}
+	public Integer getOrder_price() {
+		return order_price;
+	}
+	public void setOrder_price(Integer order_price) {
+		this.order_price = order_price;
+	}
+	public Integer getOrder_status() {
+		return order_status;
+	}
+	public void setOrder_status(Integer order_status) {
+		this.order_status = order_status;
+	}
+	public String getDeliv_addres() {
+		return deliv_addres;
+	}
+	public void setDeliv_addres(String deliv_addres) {
+		this.deliv_addres = deliv_addres;
+	}
+	public Integer getOrder_pstatus() {
+		return order_pstatus;
+	}
+	public void setOrder_pstatus(Integer order_pstatus) {
+		this.order_pstatus = order_pstatus;
+	}
+}
