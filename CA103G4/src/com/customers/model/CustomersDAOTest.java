e601114a3bb1fd8e83ac2822f06c57e3c9e0560e
b2778173 <40678827+b2778173@users.noreply.github.com>
2018-09-10 21:47:48 +0800

    Customres Jedis穩定版本。
    0910

--- CA103G4/src/com/customers/model/CustomersDAOTest.java
+++ CA103G4/src/com/customers/model/CustomersDAOTest.java
@@ -8,7 +8,7 @@ public class CustomersDAOTest {
 		CustomersVO customersvo = new CustomersVO();
 		CustomersDAO dao = new CustomersDAO();
 		// insert
-		customersvo.setCus_No(Integer.toString(026));
+		customersvo.setCus_No("026");
 		customersvo.setCus_Name("Kevin_Nash");
 		customersvo.setCus_Phone("0909392246");
 		customersvo.setCus_People(3);
@@ -21,19 +21,34 @@ public class CustomersDAOTest {
 		customersvo.setCus_Phone("0800091000");
 		dao.insertOrUpdate(customersvo);
 		System.out.println("修改完成!");
+		//Query
+		System.out.println("查詢會員編號009,012:");
+		CustomersVO customersvo2 = dao.findByKey("cus:009");
+		System.out.println("候位編號: " + customersvo2.getCus_No());
+		System.out.println("客人姓名: " + customersvo2.getCus_Name());
+		System.out.println("電話: " + customersvo2.getCus_Phone());
+		System.out.println("訂位人數: " + customersvo2.getCus_People());
+		System.out.println("=========================");
+		customersvo2 = dao.findByKey("cus:012");
+		System.out.println("候位編號: " + customersvo2.getCus_No());
+		System.out.println("客人姓名: " + customersvo2.getCus_Name());
+		System.out.println("電話: " + customersvo2.getCus_Phone());
+		System.out.println("訂位人數: " + customersvo2.getCus_People());
+		System.out.println("查詢完成!");
 		// delete
-		dao.delete(006);
-
+		dao.delete("cus:006");
+		System.out.println("刪除完成!");
 		// getall
 		List<CustomersVO> list = dao.getAll();
 		for(int i =0 ; i < list.size(); i ++ ) {
-			CustomersVO customersvo2 = list.get(i);
-			System.out.println("候位編號: " + customersvo2.getCus_No());
-			System.out.println("客人姓名: " + customersvo2.getCus_Name());
-			System.out.println("電話: " + customersvo2.getCus_Phone());
-			System.out.println("訂位人數: " + customersvo2.getCus_People());
+			CustomersVO customersvo3 = list.get(i);
+			System.out.println("候位編號: " + customersvo3.getCus_No());
+			System.out.println("客人姓名: " + customersvo3.getCus_Name());
+			System.out.println("電話: " + customersvo3.getCus_Phone());
+			System.out.println("訂位人數: " + customersvo3.getCus_People());
 			System.out.println("=============================");
 		}
+		
 		
 	}
 
