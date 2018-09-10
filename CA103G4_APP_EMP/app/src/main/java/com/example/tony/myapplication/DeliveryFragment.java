c76cec5dbf6a0bb8d670e93a0da740b9e84db0a7
a3162586 <42692256+a3162586@users.noreply.github.com>
2018-09-11 01:51:09 +0800

    app

---
+++ CA103G4_APP_EMP/app/src/main/java/com/example/tony/myapplication/DeliveryFragment.java
@@ -0,0 +1,29 @@
+package com.example.tony.myapplication;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DeliveryFragment extends Fragment {
+
+
+    public DeliveryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_delivery, container, false);
+        TextView tvDelivery = view.findViewById(R.id.tvDelivery);
+        tvDelivery.setText("delivery");
+        return view;
+    }
+
+}
