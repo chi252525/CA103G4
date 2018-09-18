8b3f2e51556516d7e25ddfc67d0e40112cbc766f
Kevin <b2778173@hotmail.com>
2018-09-18 22:12:58 +0800

    correction.

--- CA103G4/src/com/member/model/MemberService.java
+++ CA103G4/src/com/member/model/MemberService.java
@@ -1,24 +1,20 @@
 package com.member.model;
 
-<<<<<<< HEAD
 import java.util.List;
-=======
 import com.post.model.PostVO;
->>>>>>> 76aa15b3fa8d21ffacc893f9093fd2232f3cd8a8
 
 public class MemberService {
-	
+
 	private MemberDAO_interface dao;
-	
+
 	public MemberService() {
 		dao = new MemberDAO();
 	}
-	
+
 	public MemberVO addMem(String mem_Id, String mem_Pw, String mem_Name, String mem_Gender, String mem_Bir,
 			String mem_Mail, String mem_Phone, String mem_Receiver, String mem_Repno, String mem_Recounty,
-			String mem_Retown, String mem_Readdr, String mem_Cardnum, String mem_Carddue,
-			 byte[] mem_Photo) {
-		
+			String mem_Retown, String mem_Readdr, String mem_Cardnum, String mem_Carddue, byte[] mem_Photo) {
+
 		MemberVO memVO = new MemberVO();
 		memVO.setMem_Id(mem_Id);
 		memVO.setMem_Pw(mem_Pw);
@@ -35,19 +31,19 @@ public class MemberService {
 		memVO.setMem_Cardnum(mem_Cardnum);
 		memVO.setMem_Carddue(mem_Carddue);
 		memVO.setMem_Photo(mem_Photo);
-		
-		dao.insert(memVO);	
-			
+
+		dao.insert(memVO);
+
 		return memVO;
 	}
-	
-	public  MemberVO updateMem(String mem_Id, String mem_Pw, String mem_Name, String mem_Gender, String mem_Bir,
+
+	public MemberVO updateMem(String mem_Id, String mem_Pw, String mem_Name, String mem_Gender, String mem_Bir,
 			String mem_Mail, String mem_Phone, String mem_Receiver, String mem_Repno, String mem_Recounty,
 			String mem_Retown, String mem_Readdr, String mem_Cardnum, String mem_Carddue, Integer mem_Bonus,
-			 byte[] mem_Photo) {
-		
+			byte[] mem_Photo) {
+
 		MemberVO memVO = new MemberVO();
-		
+
 		memVO.setMem_Id(mem_Id);
 		memVO.setMem_Pw(mem_Pw);
 		memVO.setMem_Name(mem_Name);
@@ -63,41 +59,40 @@ public class MemberService {
 		memVO.setMem_Cardnum(mem_Cardnum);
 		memVO.setMem_Carddue(mem_Carddue);
 		memVO.setMem_Bonus(mem_Bonus);
-		
-		dao.update(memVO);	
-			
+
+		dao.update(memVO);
+
 		return memVO;
 	}
-	
-	public MemberVO memVO(String mem_Id,String mem_Status) {
-		
+
+	public MemberVO memVO(String mem_Id, String mem_Status) {
+
 		MemberVO memVO = new MemberVO();
-		
+
 		memVO.setMem_Id(mem_Id);
 		memVO.setMem_Status(mem_Status);
-	
+
 		dao.changeStatus(memVO);
-		
+
 		return memVO;
 	}
-	
+
 	public MemberVO memVO(String mem_No, String mem_Name, String mem_Phone, String mem_Mail) {
-		
+
 		MemberVO memVO = new MemberVO();
 		memVO.setMem_No(mem_No);
 		memVO.setMem_Name(mem_Name);
-		
-		
+
 		return memVO;
 	}
-<<<<<<< HEAD
-	
-	public List<MemberVO> getAll(){
+
+	public List<MemberVO> getAll() {
 		return dao.getAll();
-=======
-	/*Ning add getOneMember*/
+		
+	}
+
+	/* Ning add getOneMember */
 	public MemberVO getOne_Member(String mem_No) {
 		return dao.findByPrimaryKey(mem_No);
->>>>>>> 76aa15b3fa8d21ffacc893f9093fd2232f3cd8a8
 	}
 }
