16be24b41d9d7a85ea85700e3859529d4a6f6696
bamboo <a3551343@gmail.ocm>
2018-09-23 15:14:00 +0800

    Big Test

--- CA103G4/src/com/delivery/controller/DeliveryServlet.java
+++ CA103G4/src/com/delivery/controller/DeliveryServlet.java
@@ -1,9 +1,10 @@
 package com.delivery.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.delivery.model.DeliveryService;
 import com.delivery.model.DeliveryVO;
@@ -44,8 +46,21 @@ public class DeliveryServlet extends HttpServlet {
 
 				String strdReg = "^D[-]{1}[0-9]{9}$";
 				String streReg = "^E{1}[0-9)]{9}$";
-				String strsReg = "^[12]$";
-
+				String strsReg = "^[123]$";
+				
+				//這邊是為了分頁進來時的空值問題，避免下面執行時Exception				
+				if (strd == null) {
+					strd = "";
+				}
+				if (stre == null) {
+					stre = "";
+				}
+				if (strs == null) {
+					strs = "";
+				}
+				//
+				
+				
 				if (strd.trim().length() != 0 && !strd.matches(strdReg))
 					errorMsgs.add("請檢查派送單編號的格式是否正確。");
 				if (stre.trim().length() != 0 && !stre.matches(streReg))
@@ -73,6 +88,12 @@ public class DeliveryServlet extends HttpServlet {
 				}
 				/*************************** 3.查詢完成,準備轉交(Send the Success view) *************/
 				req.getSession().setAttribute("get_By_Key", delVOList);
+				req.setAttribute("deln", strd);
+				req.setAttribute("empn", stre);
+				req.setAttribute("dels", strs);
+
+				
+				
 				String url = "select_page.jsp";
 				RequestDispatcher successView = req.getRequestDispatcher(url);
 				successView.forward(req, res);
@@ -85,41 +106,41 @@ public class DeliveryServlet extends HttpServlet {
 		}
 
 		if ("update".equals(action)) { // 更新派送單狀態
+			
+			/*************************** 1.接收請求參數 - 輸入格式的錯誤處理 **********************/
 
-	
-				/*************************** 1.接收請求參數 - 輸入格式的錯誤處理 **********************/
-				
-				String emp = new String(req.getParameter("emp_no"));
-				String status = new String(req.getParameter("deliv_status"));
-				String deliv = new String(req.getParameter("deliv_no"));				
-				
-				if ("2".equals(status)) {
-					status = "3";
-				}
-				
-				 if ("1".equals(status)) {
-					status = "2";
-				}
-				
+			String emp = new String(req.getParameter("emp_no"));
+			String status = new String(req.getParameter("deliv_status"));
+			String deliv = new String(req.getParameter("deliv_no"));
 
-				DeliveryVO delVO = new DeliveryVO();
-				delVO.setEmp_no(emp);
-				delVO.setDeliv_status(status);
-				delVO.setDeliv_no(deliv);
+			if ("2".equals(status)) {
+				status = "3";
+			}
 
-				/*************************** 2.開始修改資料 ***************************************/
-				DeliveryService delSvc = new DeliveryService();
-				delVO = delSvc.updateDelivery(emp, status, deliv);
-				
-				/*************************** 3.刪除完成,準備轉交(Send the Success view) ***********/
-				req.setAttribute("update",delVO);
-				
-				String url = "select_page.jsp";
-				RequestDispatcher successView = req.getRequestDispatcher(url);   // 修改成功後,轉交回送出修改的來源網頁
-				successView.forward(req, res);
+			if ("1".equals(status)) {
+				status = "2";
+			}
 
-				/*************************** 其他可能的錯誤處理 **********************************/
-		
+			DeliveryVO delVO = new DeliveryVO();
+			delVO.setEmp_no(emp);
+			delVO.setDeliv_status(status);
+			delVO.setDeliv_no(deliv);
+
+			/*************************** 2.開始修改資料 ***************************************/
+			DeliveryService delSvc = new DeliveryService();
+			delVO = delSvc.updateDelivery(emp, status, deliv);
+
+			/*************************** 3.刪除完成,準備轉交(Send the Success view) ***********/
+			req.setAttribute("update", delVO);
+			
+			String requestURL = req.getParameter("requestURL");
+			System.out.println(requestURL);
+			String url = requestURL;
+//			String url = "select_page.jsp";
+			RequestDispatcher successView = req.getRequestDispatcher(url); // 修改成功後,轉交回送出修改的來源網頁
+			successView.forward(req, res);
+
+			/*************************** 其他可能的錯誤處理 **********************************/
 
 		}
 
@@ -128,22 +149,22 @@ public class DeliveryServlet extends HttpServlet {
 			/*********************** 1.接收請求參數 - 輸入格式的錯誤處理 *************************/
 			List<String> errorMsgs = new LinkedList<String>();
 			req.setAttribute("errorMsgs", errorMsgs);
-			
+
 			String branchno = req.getParameter("branch_no").trim();
 			String branchnoReg = "^[0-9]{4}$";
-			
+
 			if (branchno.trim().length() != 0 && !branchno.matches(branchnoReg)) {
 				errorMsgs.add("請確認格式");
 			} else if (branchno.trim().length() == 0) {
 				errorMsgs.add("請填寫資料");
-			} 
+			}
 
 			if (!errorMsgs.isEmpty()) {
 				RequestDispatcher failureView = req.getRequestDispatcher("select_page.jsp");
 				failureView.forward(req, res);
 				return;// 程式中斷
 			}
-			
+
 			DeliveryVO delVO = new DeliveryVO();
 //			delVO.setBranch_no(branchno);
 //				delVO.setEmp_no(emp);
@@ -152,8 +173,8 @@ public class DeliveryServlet extends HttpServlet {
 			/*************************** 2.開始新增資料 ***************************************/
 			try {
 				DeliveryService delSvc = new DeliveryService();
-				delVO = delSvc.addDelivery(branchno);	
-			} catch(Exception e) {
+				delVO = delSvc.addDelivery(branchno);
+			} catch (Exception e) {
 				errorMsgs.add("請確定是否有此分店編號！");
 				if (!errorMsgs.isEmpty()) {
 					RequestDispatcher failureView = req.getRequestDispatcher("select_page.jsp");
@@ -161,8 +182,8 @@ public class DeliveryServlet extends HttpServlet {
 					return;// 程式中斷
 				}
 			}
-			
-			req.setAttribute("insert",delVO);
+
+			req.setAttribute("insert", delVO);
 
 			/*************************** 3.新增完成,準備轉交(Send the Success view) ***********/
 			String url = "select_page.jsp";
