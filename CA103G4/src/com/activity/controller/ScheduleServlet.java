5c07f2248c2375d9b932703806bac0b7c5507688
chi252525 <chuenningyeh@gmail.com>
2018-10-07 23:05:04 +0800

    update fix cat

--- CA103G4/src/com/activity/controller/ScheduleServlet.java
+++ CA103G4/src/com/activity/controller/ScheduleServlet.java
@@ -1,5 +1,43 @@
 package com.activity.controller;
 
-public class ScheduleServlet {
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ScheduleServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public ScheduleServlet () {
+        super();
+    }
+ 
+    Timer timer ; 
+    
+   public void init() {
+	   timer = new Timer();
+		SimpleDateFormat time_format = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");  
+	   
+		TimerTask task = new TimerTask() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			
+			
+		};
+	   
+	   
+   }
+	
+	
+	
+	
 }
