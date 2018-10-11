918b1a75c743ce9298db2ad64ef55adc0cc6d119
chuenning <chuenningyeh@gmail.com>
2018-10-11 14:06:22 +0800

    update scheduleServlet

--- CA103G4/src/com/menu/controller/MenuShowImage.java
+++ CA103G4/src/com/menu/controller/MenuShowImage.java
@@ -1,9 +1,12 @@
 package com.menu.controller;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.naming.Context;
 import javax.naming.NamingException;
@@ -27,11 +30,28 @@ public class MenuShowImage extends HttpServlet{
 		ServletOutputStream out = res.getOutputStream();
 	
 		try{
-//			System.out.println("in PostShowImage");
-			String menu_No = req.getParameter("menu_No");
-			MenuService menuSvc = new MenuService();
-			byte[] image = menuSvc.getOneMenu(menu_No).getMenu_Photo();
-			out.write(image);
+			Statement stmt = con.createStatement();
+			String menu_No = req.getParameter("menu_No").trim();
+			ResultSet rs = stmt.executeQuery(
+					"select MENU_PHOTO from MENU where MENU_NO='"+menu_No+"'");
+			if (rs.next()) {
+				BufferedInputStream in = new BufferedInputStream(rs.getBinaryStream("MENU_PHOTO"));
+				byte[] buf = new byte[4 * 1024]; // 4K buffer
+				int len;
+				while ((len = in.read(buf)) != -1) {
+					out.write(buf, 0, len);
+				}
+				in.close();
+			} else {
+				//res.sendError(HttpServletResponse.SC_NOT_FOUND);
+				InputStream in = getServletContext().getResourceAsStream("/res/img/ad_no_photo.png");
+				byte[] buf = new byte[in.available()];
+				in.read(buf);
+				out.write(buf);
+				in.close();
+			}
+			rs.close();
+			stmt.close();
 		}	catch (Exception e){
 			InputStream in = getServletContext().getResourceAsStream("/res/img/menu_no_photo.jpg");
 			byte[] b = new byte[in.available()];
