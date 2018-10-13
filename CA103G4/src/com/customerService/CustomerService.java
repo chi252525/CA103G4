79294d69d9a083d12ff6d15daebdd765e27e183e
Kevin <b2778173@hotmail.com>
2018-10-14 03:27:12 +0800

    customer service final version.

--- CA103G4/src/com/customerService/CustomerService.java
+++ CA103G4/src/com/customerService/CustomerService.java
@@ -1,9 +1,12 @@
 
-
 package com.customerService;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.websocket.CloseReason;
@@ -15,15 +18,21 @@ import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
+import org.json.JSONObject;
+
 @ServerEndpoint("/CustomerService/{username}/{chatroom}")
 public class CustomerService {
 
 	private static final Set<Session> allsession = Collections.synchronizedSet(new HashSet<Session>());
 
 	@OnOpen
-	public void onOpen(@PathParam("username") String username, Session usersession, @PathParam("chatroom")String chatroom) {
-		allsession.add(usersession);
-		System.out.println("SessionID " + usersession.getId() + " with userName: " + username + " connected! in the "+ chatroom);
+	public void onOpen(@PathParam("username") String username, Session usersession,
+			@PathParam("chatroom") String chatroom) {
+		if (allsession.size() < 2)//限制為兩個session
+			allsession.add(usersession);
+		System.out.println("目前客服 " + allsession.size() + " 人");
+		System.out.println(
+				"SessionID " + usersession.getId() + " with userName: " + username + " connected! in the " + chatroom);
 //		for (Session s : allsession) {
 //			if (s.isOpen()) {
 //				s.getAsyncRemote().sendText("{'username':" + username + ", 'message':' �[�J��� !','type':'sysMsg'}");
@@ -34,13 +43,26 @@ public class CustomerService {
 	}
 
 	@OnMessage
-	public void onMessage(Session user, String msg) {
-		for (Session s : allsession) {
-			if (s.isOpen()) {
-				s.getAsyncRemote().sendText(msg);
-			}
+	public void onMessage(Session user, String msg, @PathParam("username") String username) throws IOException {
+		ArrayList<Session> a = new ArrayList<Session>();//用來存session 的list物件
+		
+		for (Session s : allsession) {//將所有session加入arraylist
+			a.add(s);
 		}
+		for (int i = 0; i < allsession.size(); i++) {
+			for (int z = 0; z < a.size(); z++) {
+				if (user.getId().equals(a.get(z).getId())) { //如果發訊息的session與allsession裡面任一相同就發訊息給allsession裡所有session
+					if (a.get(i).isOpen()) {
+						a.get(i).getBasicRemote().sendText(msg);
+					}
+				}
+
+			}
+
+		}
+
 		System.out.println("Message received: " + msg);
+
 	}
 
 	@OnError
@@ -50,17 +72,21 @@ public class CustomerService {
 
 	@OnClose
 	public void onClose(Session usersession, CloseReason reason, @PathParam("username") String username) {
-		allsession.remove(usersession);
+		ArrayList<Session> a = new ArrayList<Session>();//用來存session 的list物件
+		for (Session s : allsession) {//將所有session加入arraylist
+			a.add(s);
+		}
 		for (Session s : allsession) {
-			if (s.isOpen()) {
-				//有人離線推播系統訊息
-				s.getAsyncRemote().sendText("{\"username\":\""+username+"\",\"type\":\"sysMsg\",\"message\":\" 離開客服系統!\"}");
+			for (int z = 0; z < a.size(); z++) {
+				if (usersession.getId().equals(a.get(z).getId())) { 
+					if (s.isOpen()) {
+						s.getAsyncRemote().sendText("{\"username\":\"" + username + "\",\"type\":\"sysMsg\",\"message\":\" 離開客服系統!\"}");;
+					}
+				}
 			}
 		}
-		
-		
+		System.out.println(allsession.remove(usersession));//發送離開消息後再移除
 		System.out.println("SessionID: " + usersession.getId() + " userName: " + username + " : " + "IP位址: "
 				+ usersession.getRequestURI() + " 離開了 " + reason.getCloseCode().getCode());
 	}
-
 }
