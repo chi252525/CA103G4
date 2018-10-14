c836050fea4ec90685b10cb0be2ce55ee7a16f38
chiapao <abow129@gmail.com>
2018-10-15 01:32:57 +0800

    for HQ login

---
+++ CA103G4/src/com/generalmanager/controller/GeneralmanagerServlet.java
@@ -0,0 +1,70 @@
+package com.generalmanager.controller;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+import com.generalmanager.model.GeneralmanagerService;
+import com.generalmanager.model.GeneralmanagerVO;
+
+public class GeneralmanagerServlet extends HttpServlet{
+	
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException {
+		doPost(req,res);
+	}
+	
+	public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException {
+		
+		req.setCharacterEncoding("UTF-8");
+		String action = req.getParameter("action");
+		
+		if("loginHQ".equals(action)) {
+			List<String> errorMsgs = new LinkedList<>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			try {
+				String mger_Acnum = req.getParameter("mger_Acnum");
+				String mger_Psw = req.getParameter("mger_Psw");
+				
+				GeneralmanagerService mgerSvc = new GeneralmanagerService();
+				GeneralmanagerVO mgerVO = new GeneralmanagerVO();
+				mgerVO = mgerSvc.getOneByAcnum(mger_Acnum);
+				
+				if(mger_Acnum.trim().isEmpty() || mger_Psw.trim().isEmpty()) {
+					errorMsgs.add("尚未輸入帳號或密碼");
+				}else if(mgerVO != null){
+					System.out.println(mgerVO.getMger_Psw());
+					if(!mgerVO.getMger_Psw().equals(mgerSvc)) {
+						errorMsgs.add("密碼錯誤");
+					}
+				}else {
+					errorMsgs.add("無此帳號");
+				}
+				
+				if(!errorMsgs.isEmpty()) {
+					req.setAttribute("mgerVO", mgerVO);  // 含有輸入格式錯誤的mgerVO物件,也存入req
+					RequestDispatcher failureView = req.getRequestDispatcher("/back_end/employee/empLogin_HQ.jsp");
+					failureView.forward(req, res);
+					return; //程式中斷
+				}
+					
+				HttpSession session = req.getSession();
+				session.setAttribute("mgerVO", mgerVO);
+				
+				
+			}catch(Exception e) {
+				errorMsgs.add("登入失敗"+e.getMessage());
+				RequestDispatcher failuerView = req.getRequestDispatcher("/back_end/employee/empLogin_HQ.jsp");
+				failuerView.forward(req, res);
+			}
+			
+		
+		}
+		
+	}
+
+}
