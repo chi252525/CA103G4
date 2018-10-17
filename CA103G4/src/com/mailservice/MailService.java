43796ff61c15f5f326307755840633bbded2b197
Kevin <b2778173@hotmail.com>
2018-10-17 20:53:47 +0800

    Update CA103G4/src/com/mailservice/MailService.java

--- CA103G4/src/com/mailservice/MailService.java
+++ CA103G4/src/com/mailservice/MailService.java
@@ -29,7 +29,7 @@ public class MailService {
        // ●設定 gmail 的帳號 & 密碼 (將藉由你的Gmail來傳送Email)
        // ●須將myGmail的【安全性較低的應用程式存取權】打開
 	     final String myGmail = "b2778173@gmail.com";
-	     final String myGmail_password = "Birmingham2014";
+	     final String myGmail_password = "a2765811";
 		   Session session = Session.getInstance(props, new Authenticator() {
 			   protected PasswordAuthentication getPasswordAuthentication() {
 				   return new PasswordAuthentication(myGmail, myGmail_password);
